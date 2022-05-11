fdfdeb3f73f53d0bf5b57ae2802ba989076f872b
Quang <69511888+QuangNguyen4673@users.noreply.github.com>
2022-05-11 16:00:18 +0700

    fixing tests

--- src/components/Navbar/__test__/Navbar.test.js
+++ src/components/Navbar/__test__/Navbar.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react"
 import Navbar from "../index.js"
-
-it("renders Navbar burger icon", () => {
-  render(<Navbar />)
-  const element = screen.getByTestId("burger")
-  expect(element).toBeInTheDocument()
+describe("Testing sum1", () => {
+  it("renders Navbar burger icon", () => {
+    render(<Navbar />)
+    const element = screen.getByTestId("burger")
+    expect(element).toBeInTheDocument()
+  })
 })
--- src/components/Weather/__test__/Weather.test.js
+++ src/components/Weather/__test__/Weather.test.js
@@ -1,8 +1,10 @@
 import { render, screen, waitFor } from "@testing-library/react"
 import Weather from "../index.js"
 
-it("renders weather status", async () => {
-  render(<Weather />)
-  const element = screen.getByTestId("weather-status")
-  await waitFor(() => expect(element).toHaveTextContent(/cloudy/i))
+describe("Testing sum4", () => {
+  it("renders weather status", async () => {
+    render(<Weather />)
+    const element = screen.getByTestId("weather-status")
+    await waitFor(() => expect(element).toHaveTextContent(/cloudy/i))
+  })
 })
