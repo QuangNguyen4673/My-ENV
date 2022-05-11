dccdeb604dcfe927aa1b6ebe00ada58c2d72532f
Quang <69511888+QuangNguyen4673@users.noreply.github.com>
2022-05-11 15:15:27 +0700

    add test for top-text

--- src/components/Chart/__test__/Chart.test.js
+++ src/components/Chart/__test__/Chart.test.js
@@ -1,17 +1,20 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import Chart from "../index.js"
 
-it("renders Chart visible", () => {
-  render(<Chart />)
-  const element = screen.getByTestId("weather-chart")
-  const svgElement = screen.getByTestId("weather-chart-svg")
+describe("Testing sum", () => {
+  it("renders Chart visible", () => {
+    render(<Chart />)
+    const element = screen.getByTestId("weather-chart")
+    const svgElement = screen.getByTestId("weather-chart-svg")
 
-  expect(element).toContainElement(svgElement)
-  expect(svgElement).toBeInTheDocument()
-})
+    expect(element).toContainElement(svgElement)
+    expect(svgElement).toBeInTheDocument()
+  })
 
-fit("renders top text visible", async () => {
-  const { container } = render(<Chart />)
-  const topText = container.querySelector('[class="weather-chart"]')
-  await waitFor(() => expect(topText).toBeInTheDocument())
+  it("renders top text visible", async () => {
+    const { container } = render(<Chart />)
+    await waitFor(() =>
+      expect(container.querySelector('[class="top-text"]')).toBeInTheDocument()
+    )
+  })
 })
